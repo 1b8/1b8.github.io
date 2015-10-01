6063a70a3db2855eb2e526125790f849a8f8d868
1b8 <kronosville@gmail.com>
2015-10-02 12:57:12 +1300

    Rewrite /stuff/acodes/

--- stuff/acodes/acodes.js
+++ stuff/acodes/acodes.js
@@ -1,178 +1,88 @@
 // Minecraft Bukkit Essentials & codes, JavaScript
 
-var black = "#000000";
-var aCodes = [
-	{
-		code: "0",
-		type: "colour",
-		colour: black
-	},
-	{
-		code: "1",
-		type: "colour",
-		colour: "#0000AA"
-	},
-	{
-		code: "2",
-		type: "colour",
-		colour: "00AA00"
-	},
-	{
-		code: "3",
-		type: "colour",
-		colour: "#00AAAA"
-	},
-	{
-		code: "4",
-		type: "colour",
-		colour: "#AA0000"
-	},
-	{
-		code: "5",
-		type: "colour",
-		colour: "#AA00AA"
-	},
-	{
-		code: "6",
-		type: "colour",
-		colour: "#FFAA00"
-	},
-	{
-		code: "7",
-		type: "colour",
-		colour: "#AAAAAA"
-	},
-	{
-		code: "8",
-		type: "colour",
-		colour: "#555555"
-	},
-	{
-		code: "9",
-		type: "colour",
-		colour: "#5555FF"
-	},
-	{
-		code: "a",
-		type: "colour",
-		colour: "#55FF55"
-	},
-	{
-		code: "b",
-		type: "colour",
-		colour: "#55FFFF"
-	},
-	{
-		code: "c",
-		type: "colour",
-		colour: "#FF5555"
-	},
-	{
-		code: "d",
-		type: "colour",
-		colour: "#FF55FF"
-	},
-	{
-		code: "e",
-		type: "colour",
-		colour: "#FFFF55"
-	},
-	{
-		code: "f",
-		type: "colour",
-		colour: "#FFFFFF"
-	},
-	{
-		code: "l",
-		css: "text-weight:bold;"
-	},
-	{
-		code: "m",
-		css: "text-decoration:line-through;"
-	},
-	{
-		code: "n",
-		css: "text-decoration:underline;"
-	},
-	{
-		code: "o",
-		css: "font-style:italic;"
-	},
-	{
-		code: "r",
-		type: "colour"
-	}
-];
-
-function aCodesToHTML(input) {
-	function closeSpans() {
-		for(var i = 0; i < spansOpen; i++) {
-			output += "</span>";
-		}
-		spansOpen = 0;
-	}
-
-	function addLetterToOutput() {
-		output += input[i];
-	}
-
-	var spansOpen = 0;
-	var output = "";
-
-	for(var i = 0; i < input.length; i++) {
-
-		if(input[i] === "&") {
-			var foundCode = false;
-			for(var j = 0; j < aCodes.length; j++) {
-				var aCode = aCodes[j];
-				if(input[i+1].toLowerCase() == aCode.code) {
-
-					foundCode = true;
-					if(aCode.type === "colour") {
-						closeSpans();
-						if (aCode.code !== "r") {
-							output += '<span style="color:' + aCode.colour + ';">';
-							spansOpen++;
-						}
-					} else {
-						output += '<span style="' + aCode.css + '">';
-						spansOpen++;
-					}
-					i++; // Skip to the next char
-					break;
-				}
-			}
-			if(!foundCode) {
-				addLetterToOutput();
-			}
-		} else {
-			addLetterToOutput();
-		}
-		if(i === input.length-1) {
-			closeSpans();
-		}
-	}
-
-	return output;
+function hex(val) {
+  return 'color:#' + val;
 }
 
+function decor(val) {
+  return 'text-decoration:' + val;
+}
+
+function aCodesToHTML (input, startCode) {
+
+  var spansOpen = 0;
+
+  function closeSpans() {
+    var result = '';
+    for(var i = 0; i < spansOpen; i++) {
+      // Better performance than `+=`
+      result = result + '</span>';
+    }
+    return result;
+  }
+
+  function aCodeRegExp(val) {
+    return new RegExp(startCode + val, 'gi');
+  }
+
+  return input.replace(aCodeRegExp('([\\da-fk-or])'), function (full, code) {
+
+    var willCloseSpans = true, lower = code.toLowerCase(), css, result = '';
+
+    switch (lower) {
+      case '0': css = hex('000000'); break;
+      case '1': css = hex('0000AA'); break;
+      case '2': css = hex('00AA00'); break;
+      case '3': css = hex('00AAAA'); break;
+      case '4': css = hex('AA0000'); break;
+      case '5': css = hex('AA00AA'); break;
+      case '6': css = hex('FFAA00'); break;
+      case '7': css = hex('AAAAAA'); break;
+      case '8': css = hex('555555'); break;
+      case '9': css = hex('5555FF'); break;
+      case 'a': css = hex('55FF55'); break;
+      case 'b': css = hex('55FFFF'); break;
+      case 'c': css = hex('FF5555'); break;
+      case 'd': css = hex('FF55FF'); break;
+      case 'e': css = hex('FFFF55'); break;
+      case 'f': css = hex('FFFFFF'); break;
+      // case 'k': css = '...'; willCloseSpans = false; break;
+      case 'l': css = 'font-weight:bold'; willCloseSpans = false; break;
+      case 'm': css = decor('line-through'); willCloseSpans = false; break;
+      case 'n': css = decor('underline'); willCloseSpans = false; break;
+      case 'o': css = 'font-style:italic'; willCloseSpans = false; break;
+    }
+
+    if (willCloseSpans) {
+      result = closeSpans();
+      spansOpen = 0;
+    }
+
+    if (lower !== 'r') {
+      result = result + '<span style="' + css + ';">'; // Better performance than `+=`
+      spansOpen++;
+    }
+    return result;
+
+  }) + closeSpans(); // Close spans at the end
+}
 
 /* Check if "/stuff/acodes" is within the path.
 This file is also used in http://kronosville.net/staff
 It doesn't just check if the first bit IS /stuff/acodes
 so that it can be tested without it being on the site. */
 
-if (location.pathname.indexOf("/stuff/acodes") !== -1) {
+if (window.location.pathname.indexOf('/stuff/acodes') !== -1) {
 
-	function el(id) {
-		return document.getElementById("acodes-" + id);
-	}
+  function el(id) {
+    return document.getElementById('acodes-' + id);
+  }
 
-	el("btn").onclick = function() {
-		var result = aCodesToHTML(el("input").value);
-		el("output").value = result;
-		el("preview").innerHTML = 'Preview: <span style="font-family:monospace;">'
-			+ result + '</span>';
-	};
+  el('btn').onclick = function() {
+    var result = aCodesToHTML(el('input').value, '&');
+    el('output').value = result;
+    el('preview').innerHTML = 'Preview: <span style="font-family:monospace;">'
+    + result + '</span>';
+  };
 
 }
